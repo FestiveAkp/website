1817adc62d4faf94bd46a157c9406a78674f9751
Akash Pillai <akashp807@gmail.com>
2020-03-23 03:06:34 -0500

    Add PurgeCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,13 @@
 module.exports = {
     plugins: [
         require('tailwindcss'),
-        require('autoprefixer')
+        require('autoprefixer'),
+        process.env.NODE_ENV === 'production' && require('@fullhuman/postcss-purgecss')({
+            content: [
+                './index.html',
+                './projects/index.html'
+            ],
+            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+        })
     ]
 }
