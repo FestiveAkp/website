6256dca959d2119fe362ef2095077ecabec252d9
Akash Pillai <akashp807@gmail.com>
2020-03-23 03:09:55 -0500

    Upload progress

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                'primary': '#BB86FC',
+                'primary-bg': '#211D27'
+            },
+            fontFamily: {
+                body: ['"Source Sans Pro"', 'sans-serif']
+            }
+        },
+    },
+    variants: {
+        backgroundColor: ['responsive', 'hover', 'focus', 'active']
+    },
+    plugins: [],
+}
