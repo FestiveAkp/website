abec00214af17b8b58881b92830d29104d0b7490
Akash Pillai <akashp807@gmail.com>
2020-08-18 13:34:57 -0500

    Insert README in output repo

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,18 @@ set -e
 rm -rf dist
 npm run build
 
-# stage files
+# write and stage files
 cd dist
 git init
 echo "akash.dev" > CNAME
+
+cat << EOL > README.md
+# festiveakp.github.io
+This repo holds the static build output for my personal website, which is deployed through GitHub Pages.
+
+You can find the actual source repo [here](https://github.com/FestiveAkp/website).
+EOL
+
 git add -A
 git commit -m "deploy: $(date +"%F %r")"
 
